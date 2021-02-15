283eec1e6d708714fed597faf09abbcd0c462529
Temple Okosun <okosun.temple@gmail.com>
2021-02-15 10:22:11 -0500

    added-rbac

--- contracts/escrowRBAC.sol
+++ contracts/escrowRBAC.sol
@@ -3,7 +3,9 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract escrowRBAC {
+contract escrowRBAC is AccessControl {
+  bytes32 public constant BUYER_ROLE = keccak256("BUYER_ROLE");
+  bytes32 public constant SELLER_ROLE = keccak256("SELLER_ROLE");
 
   enum State { AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETE }
 
@@ -12,24 +14,28 @@ contract escrowRBAC {
   address public buyer;
   address payable public seller;
 
-  modifier onlyBuyer() {
-    require(msg.sender == buyer, "Only buyer can call this method");
-    _;
-  }
 
   constructor(address _buyer, address payable _seller) public {
-    buyer = _buyer;
-    seller = _seller;
+    _setupRole(BUYER_ROLE, _buyer);
+    _setupRole(SELLER_ROLE, _seller);
   }
 
-  function deposit() onlyBuyer external payable {
+  function deposit() external payable {
+    require(hasRole(BUYER_ROLE, msg.sender), "Caller is not a buyer");
     require(currState == State.AWAITING_PAYMENT, "Already paid");
     currState = State.AWAITING_DELIVERY;
   }
 
-  function confirmDelivery() onlyBuyer external {
+  function confirmDelivery() external {
+    require(hasRole(BUYER_ROLE, msg.sender), "Caller is not a buyer");
     require(currState == State.AWAITING_DELIVERY, "Cannot confirm delivery");
-    seller.transfer(address(this).balance);
+
     currState = State.COMPLETE;
+  }
+
+  function withdrawPayment() external{
+    require(hasRole(SELLER_ROLE, msg.sender), "Caller is not a seller");
+    require(currState == State.COMPLETE, "Cannot confirm item has been received");
+    seller.transfer(address(this).balance);
   }
 }
